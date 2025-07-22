9e3c8b03efc8855b89348142366ba7ab8c20c21a
Nezax09 <higgmangeecd@gmail.com>
2025-07-22 18:35:44 -0500

    Update blacknet-themes.lua

--- blacknet-themes.lua
+++ blacknet-themes.lua
@@ -1,5 +1,3 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
 local Blacknet = {
     TextColor = Color3.fromHex("#ffffff"),
     Background = Color3.fromHex("#5a0000"),
