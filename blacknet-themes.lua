5a8b95bd2d9b59a7ba3cc630ef58e924b5daa785
Nezax09 <higgmangeecd@gmail.com>
2025-07-22 18:28:52 -0500

    Update and rename blacknet-themes to blacknet-themes.lua

---
+++ blacknet-themes.lua
@@ -0,0 +1,165 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Blacknet = {
+    TextColor = Color3.fromHex("#ffffff"),
+    Background = Color3.fromHex("#5a0000"),
+    Topbar = Color3.fromHex("#350000"),
+    Shadow = Color3.fromHex("#1a0000"),
+
+    NotificationBackground = Color3.fromHex("#5a0000"),
+    NotificationActionsBackground = Color3.fromHex("#ff0000"),
+
+    TabBackground = Color3.fromHex("#350000"),
+    TabStroke = Color3.fromHex("#ff0000"),
+    TabBackgroundSelected = Color3.fromHex("#ff0000"),
+    TabTextColor = Color3.fromHex("#ffffff"),
+    SelectedTabTextColor = Color3.fromHex("#350000"),
+
+    ElementBackground = Color3.fromHex("#350000"),
+    ElementBackgroundHover = Color3.fromHex("#4a0000"),
+    SecondaryElementBackground = Color3.fromHex("#5a0000"),
+    ElementStroke = Color3.fromHex("#ff0000"),
+    SecondaryElementStroke = Color3.fromHex("#350000"),
+
+    SliderBackground = Color3.fromHex("#ff0000"),
+    SliderProgress = Color3.fromHex("#ff0000"),
+    SliderStroke = Color3.fromHex("#ff0000"),
+
+    ToggleBackground = Color3.fromHex("#350000"),
+    ToggleEnabled = Color3.fromHex("#ff0000"),
+    ToggleDisabled = Color3.fromRGB(100, 100, 100),
+    ToggleEnabledStroke = Color3.fromHex("#ff0000"),
+    ToggleDisabledStroke = Color3.fromRGB(125, 125, 125),
+    ToggleEnabledOuterStroke = Color3.fromRGB(100, 100, 100),
+    ToggleDisabledOuterStroke = Color3.fromRGB(65, 65, 65),
+
+    DropdownSelected = Color3.fromHex("#4a0000"),
+    DropdownUnselected = Color3.fromHex("#350000"),
+
+    InputBackground = Color3.fromHex("#350000"),
+    InputStroke = Color3.fromHex("#ff0000"),
+    PlaceholderColor = Color3.fromHex("#ffffff")
+}
+
+local Cataclysm = {
+    TextColor = Color3.fromHex("#000000"),
+    Background = Color3.fromHex("#ffffff"),
+    Topbar = Color3.fromHex("#dedede"),
+    Shadow = Color3.fromHex("#dedede"),
+
+    NotificationBackground = Color3.fromHex("#ffffff"),
+    NotificationActionsBackground = Color3.fromHex("#878787"),
+
+    TabBackground = Color3.fromHex("#dedede"),
+    TabStroke = Color3.fromHex("#7c7c7c"),
+    TabBackgroundSelected = Color3.fromHex("#878787"),
+    TabTextColor = Color3.fromHex("#000000"),
+    SelectedTabTextColor = Color3.fromHex("#000000"),
+
+    ElementBackground = Color3.fromHex("#dedede"),
+    ElementBackgroundHover = Color3.fromHex("#cfcfcf"),
+    SecondaryElementBackground = Color3.fromHex("#ffffff"),
+    ElementStroke = Color3.fromHex("#7c7c7c"),
+    SecondaryElementStroke = Color3.fromHex("#878787"),
+
+    SliderBackground = Color3.fromHex("#878787"),
+    SliderProgress = Color3.fromHex("#7c7c7c"),
+    SliderStroke = Color3.fromHex("#7c7c7c"),
+
+    ToggleBackground = Color3.fromHex("#dedede"),
+    ToggleEnabled = Color3.fromHex("#878787"),
+    ToggleDisabled = Color3.fromRGB(130, 130, 130),
+    ToggleEnabledStroke = Color3.fromHex("#7c7c7c"),
+    ToggleDisabledStroke = Color3.fromRGB(125, 125, 125),
+    ToggleEnabledOuterStroke = Color3.fromRGB(100, 100, 100),
+    ToggleDisabledOuterStroke = Color3.fromRGB(65, 65, 65),
+
+    DropdownSelected = Color3.fromHex("#878787"),
+    DropdownUnselected = Color3.fromHex("#dedede"),
+
+    InputBackground = Color3.fromHex("#ffffff"),
+    InputStroke = Color3.fromHex("#7c7c7c"),
+    PlaceholderColor = Color3.fromHex("#000000")
+}
+
+local Laputa = {
+    TextColor = Color3.fromHex("#ffffff"),
+    Background = Color3.fromHex("#173839"),
+    Topbar = Color3.fromHex("#004b55"),
+    Shadow = Color3.fromHex("#0d1c1d"),
+
+    NotificationBackground = Color3.fromHex("#173839"),
+    NotificationActionsBackground = Color3.fromHex("#00f7ff"),
+
+    TabBackground = Color3.fromHex("#116265"),
+    TabStroke = Color3.fromHex("#00f7ff"),
+    TabBackgroundSelected = Color3.fromHex("#00f7ff"),
+    TabTextColor = Color3.fromHex("#ffffff"),
+    SelectedTabTextColor = Color3.fromHex("#004b55"),
+
+    ElementBackground = Color3.fromHex("#004b55"),
+    ElementBackgroundHover = Color3.fromHex("#116265"),
+    SecondaryElementBackground = Color3.fromHex("#173839"),
+    ElementStroke = Color3.fromHex("#00f7ff"),
+    SecondaryElementStroke = Color3.fromHex("#116265"),
+
+    SliderBackground = Color3.fromHex("#116265"),
+    SliderProgress = Color3.fromHex("#00f7ff"),
+    SliderStroke = Color3.fromHex("#00f7ff"),
+
+    ToggleBackground = Color3.fromHex("#004b55"),
+    ToggleEnabled = Color3.fromHex("#00f7ff"),
+    ToggleDisabled = Color3.fromRGB(100, 100, 100),
+    ToggleEnabledStroke = Color3.fromHex("#00f7ff"),
+    ToggleDisabledStroke = Color3.fromRGB(125, 125, 125),
+    ToggleEnabledOuterStroke = Color3.fromRGB(100, 100, 100),
+    ToggleDisabledOuterStroke = Color3.fromRGB(65, 65, 65),
+
+    DropdownSelected = Color3.fromHex("#116265"),
+    DropdownUnselected = Color3.fromHex("#004b55"),
+
+    InputBackground = Color3.fromHex("#004b55"),
+    InputStroke = Color3.fromHex("#00f7ff"),
+    PlaceholderColor = Color3.fromHex("#ffffff")    
+}
+
+local Zephyr = {
+    TextColor = Color3.fromHex("#e0e0e0"),
+    Background = Color3.fromHex("#1f0020"),
+    Topbar = Color3.fromHex("#500052"),
+    Shadow = Color3.fromHex("#4b1b57"),
+
+    NotificationBackground = Color3.fromHex("#1f0020"),
+    NotificationActionsBackground = Color3.fromHex("#e300ff"),
+
+    TabBackground = Color3.fromHex("#500052"),
+    TabStroke = Color3.fromHex("#e300ff"),
+    TabBackgroundSelected = Color3.fromHex("#e300ff"),
+    TabTextColor = Color3.fromHex("#e0e0e0"),
+    SelectedTabTextColor = Color3.fromHex("#1f0020"),
+
+    ElementBackground = Color3.fromHex("#500052"),
+    ElementBackgroundHover = Color3.fromHex("#6a006e"),
+    SecondaryElementBackground = Color3.fromHex("#1f0020"),
+    ElementStroke = Color3.fromHex("#e300ff"),
+    SecondaryElementStroke = Color3.fromHex("#4b1b57"),
+
+    SliderBackground = Color3.fromHex("#e300ff"),
+    SliderProgress = Color3.fromHex("#e300ff"),
+    SliderStroke = Color3.fromHex("#e300ff"),
+
+    ToggleBackground = Color3.fromHex("#500052"),
+    ToggleEnabled = Color3.fromHex("#e300ff"),
+    ToggleDisabled = Color3.fromRGB(100, 100, 100),
+    ToggleEnabledStroke = Color3.fromHex("#e300ff"),
+    ToggleDisabledStroke = Color3.fromRGB(125, 125, 125),
+    ToggleEnabledOuterStroke = Color3.fromRGB(100, 100, 100),
+    ToggleDisabledOuterStroke = Color3.fromRGB(65, 65, 65),
+
+    DropdownSelected = Color3.fromHex("#6a006e"),
+    DropdownUnselected = Color3.fromHex("#500052"),
+
+    InputBackground = Color3.fromHex("#500052"),
+    InputStroke = Color3.fromHex("#e300ff"),
+    PlaceholderColor = Color3.fromHex("#e0e0e0")
+}
